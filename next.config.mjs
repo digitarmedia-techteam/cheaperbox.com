168dec4479677669ed4d4dca222ed15bb1f24162
ShubhamDigitar <shubham@digitarmedia.com>
2025-12-25 14:20:04 +0530

    added image domain link to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['panel.digitarmedia.com'],
+    domains: ['panel.digitarmedia.com', 'ae-pic-a1.aliexpress-media.com'],
   },
 };
 
